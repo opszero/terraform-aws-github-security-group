e0ea3ab6885947510e6933b667e61d1b08f3adc9
Thaung Htike Oo <thaunghtikeoo@Thaungs-MacBook-Air.local>
2022-09-21 02:31:46 +0630

    update security group

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 | Name | Version |
 |------|---------|
 | <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
+| <a name="provider_github"></a> [github](#provider\_github) | 5.1.0 |
 ## Inputs
 
 | Name | Description | Type | Default | Required |
@@ -15,6 +16,8 @@
 | Name | Type |
 |------|------|
 | [aws_security_group.github](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group) | resource |
+| [aws_security_group_rule.github](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule) | resource |
+| [github_ip_ranges.action](https://registry.terraform.io/providers/integrations/github/5.1.0/docs/data-sources/ip_ranges) | data source |
 ## Outputs
 
 | Name | Description |
--- main.tf
+++ main.tf
@@ -1,10 +1,30 @@
+data "github_ip_ranges" "action" {}
+
 resource "aws_security_group" "github" {
   name        = "github_meta_actions_ingress"
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "github-meta-actions"
   }
+}
+
+resource "aws_security_group_rule" "github" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = data.github_ip_ranges.action.actions_ipv4
+  ipv6_cidr_blocks  = data.github_ip_ranges.action.actions_ipv6
+  security_group_id = aws_security_group.github.id
 }
 
